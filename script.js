c3ca41fa5d005e6bd4a11dd99508600f57ee45ee
seotosna <79702470+cstps@users.noreply.github.com>
2021-09-03 04:10:41 +0000

    이동함수와 로봇의 초기 설정

--- script.js
+++ script.js
@@ -1,50 +1,101 @@
-var canvas ;
-var ctx;
-var x;
-var y ;
 
-var ballRadius = 10;
-var dx = 2;
-var dy = -2; 
-var editor;
+//전역 변수들
+let canvas ;
+let ctx;
+// Bot의 시작위치
+let imgTag = new Image();
+
+let obj ={
+  x:0,
+  y:0,
+  w:10,
+  h:10
+};
+
+// 1회 이동거리
+let dx = 10;
+let dy = 10; 
+let stopped=10;
+// CodeMirror value
+let requestId = 0;
+
+let editor;
+
+
+// 로봇의 시작위치와 크기 정의
+let bot={
+  x:0,
+  y:0,
+  w:10,
+  h:10,
+  d:0
+}
 
 window.onload = function(){
+  // canvas draw
 	canvas = document.getElementById('tutorial');
   ctx = canvas.getContext("2d");
-  x = canvas.width/2;
-  y = canvas.height-30;
+
+  initBot(bot);
+
   editor= CodeMirror.fromTextArea(document.getElementById('code'), {
     mode: "javascript",
     lineNumbers: true,
   });
   editor.save()
+  drawBot(ctx,bot);
 };
+// bot init
+function initBot(bot){
+  bot.x = 0;
+  bot.y = canvas.height-10;
+  bot.d = 0;
+  drawBot(ctx, bot);
+}
 
-function drawBall() {
-  
+function drawBot(ctx, bot) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.rect(bot.x,bot.y,obj.w,obj.h);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
-  
 }
+
 function draw() {
-    console.log(editor.getValue());
-    var txtAreaCode = editor.getValue();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    if(txtAreaCode =="move()"){
-      if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-          dx = -dx;
-      }
-      if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-          dy = -dy;
-      }
-      x += dx;
-      y += dy;
-    }
-    
+  // canvas coordinate draw
+  initBot(bot);
+  canvas_coordinate_draw(canvas.width, canvas.height);
+  console.log(editor.getValue());
+  let txtAreaCode = editor.getValue();
+  let codeline = txtAreaCode.split("\n");
+  console.log(codeline);
+  for(let i=0; i<codeline.length;i++){
+    cmd(codeline[i]);  
+  }
+}
+
+function cmd(code){
+  switch(code.trim()){
+    case "move();":
+      if(bot.d==0) bot.x+=10;
+      else if(bot.d==1) bot.y-=10;
+      else if(bot.d==2) bot.x-=10;
+      else bot.y+=10;
+      break;
+    case "turnLeft();":
+      bot.d = (bot.d+1)%4;
+    case "setColor();":
+    case "getColor();":
+    case "putObject();":
+  }
+   drawBot(ctx,bot);
+}
+
+
+// canvas coordinate draw
+function canvas_coordinate_draw(w, h){
+  //for(let x = )
 }
 
 // 배경 바닥 만들기 (가로*세로)
@@ -54,3 +105,22 @@ function draw() {
 // 소봇놓기(소봇개수, 위치, 방향)
 
 
+
+var loop = function () {
+  if (stopped>0) {
+    requestId = requestAnimationFrame(loop);
+    // just stepping the object
+    obj.x += 1;
+    stopped--;
+    drawBot(ctx, obj);
+  }
+  else {
+    stop();
+  }
+};
+
+function stop() {
+    if (requestId) {
+        window.cancelAnimationFrame(requestId);
+    }
+}
